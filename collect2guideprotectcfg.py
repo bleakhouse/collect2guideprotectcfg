69a5e30a678884968ea37eb4f9e40b351f275215
kkindof <surewin.chau@hotmail.com>
2017-05-30 16:35:28 +0800

    generate current time node

--- collect2guideprotectcfg.py
+++ collect2guideprotectcfg.py
@@ -83,6 +83,7 @@ from basedef import *
 
 def dumpsql2xml(fname='new_rule'):
 
+    curtime = time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()))
     fname = fname+time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()))+".xml"
     if os.path.isfile(fname):
         inp = raw_input('file {0} existed, ok to overwrite ? press 1 to continue\n'.format(fname))
@@ -105,6 +106,8 @@ def dumpsql2xml(fname='new_rule'):
 
     doc = ET.Element('doc')
     doc.attrib['version'] = '1.0'
+    doc.attrib['gentime'] = curtime
+
     pfp = open(purgefname,"w")
     for row in result:
         url = row[3]
